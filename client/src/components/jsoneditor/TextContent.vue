ae48a39fad75a81548c83db5cd4a3af69f8fc19b
rulkimi <azrulkimi00@gmail.com>
2025-02-25 20:00:30 +0800

    Handle newline characters in TextContent component

--- client/src/components/jsoneditor/TextContent.vue
+++ client/src/components/jsoneditor/TextContent.vue
@@ -1,13 +1,26 @@
 <script setup>
-defineProps({
+import { computed
+
+ } from 'vue';
+const props = defineProps({
   content: {
     type: Object,
     required: true
   },
 })
+
+const formattedMalay = computed(() => {
+  const text = props.content.text.malay.replace(/\\n/g, '<br />'); 
+  return text;
+})
+
+const formattedEnglish = computed(() => {
+  const text = props.content.text.english.replace(/\\n/g, '<br />');
+  return text;
+})
 </script>
 
 <template>
-  <div>{{ content.text.malay }}</div>
-  <div class="italic">{{ content.text.english }}</div>
+  <div v-html="formattedMalay"></div>
+  <div class="italic" v-html="formattedEnglish"></div>
 </template>
