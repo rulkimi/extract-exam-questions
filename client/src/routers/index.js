cd03246dadbc90857bf18cbb55700045c2b6ad7e
eemauuu <ikmalrasli@gmail.com>
2025-08-15 12:08:33 +0800

    changed to createWebHashHistory() on router

--- client/src/routers/index.js
+++ client/src/routers/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
   { path: '/', redirect: '/docs' },
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
