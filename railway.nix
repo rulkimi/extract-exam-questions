2837c9d20e86a0550cfeb6c0a68a3514785ce992
eemauuu <ikmalrasli@gmail.com>
2025-09-17 00:29:51 +0800

    using opencv headless version

--- railway.nix
+++
@@ -1,9 +0,0 @@
-# railway.nix
-{ pkgs, ... }: {
-  # Add system-level packages here.
-  # For example, to install libgl1 and libsm6 for OpenCV.
-  environment.systemPackages = with pkgs; [
-    libgl1
-    libsm6
-  ];
-}
